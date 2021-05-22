3f3fe455c2bab79159f2a070f071ed58f8a08d3c
Akshay Dohroo <akshay.dohroo3@gmail.com>
2021-05-22 17:45:44 +0530

    Search Bar Building

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import SearchBar from "./SearchBar/SearchBar";
+import Container from "@material-ui/core/Container";
+class App extends React.Component {
+  constructor(props) {
+    super(props);
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <Container>
+        <SearchBar />
+      </Container>
+    );
+  }
 }
 
 export default App;
